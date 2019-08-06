471fb9424af4d192620f43f685d538d255d414f7
Chuck Ha <chuckh@vmware.com>
2019-08-06 14:43:53 -0400

    add reset functionality for aws
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

--- devenv/cloudformation/reset.sh
+++ devenv/cloudformation/reset.sh
@@ -4,7 +4,13 @@ set -o xtrace
 set -o nounset
 set -o errexit
 
-INSTANCE_ID=$(aws cloudformation describe-stacks --stack-name ClusterAPIDevEnvironment --query "Stacks[0].Outputs[0].OutputValue" --output=text)
-INSTANCE_DNS=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].PublicDnsName" --output text)
+alias ssh='ssh -o "StrictHostKeyChecking no'
 
-ssh "ubuntu@${INSTANCE_DNS}" 'sudo kubeadm reset -f --cri-socket /var/run/containerd/containerd.sock && sudo kubeadm init --config /tmp/kubeadm-config.yaml'
+INSTANCE_ID=$(aws cloudformation describe-stacks --stack-name ClusterAPIDevEnvironment --query "Stacks[0].Outputs[0].OutputValue" --output=text)
+INSTANCE_IP=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+
+ssh "ubuntu@${INSTANCE_IP}" 'sudo kubeadm reset -f --cri-socket /var/run/containerd/containerd.sock && sudo kubeadm init --config /tmp/kubeadm-config.yaml'
+ssh "ubuntu@${INSTANCE_IP}" 'sudo -s cat /etc/kubernetes/admin.conf' > dev-kubeconfig
+
+sed -i '' -E 's/([0-9]{1,3}\.){3}[0-9]{1,3}/'$INSTANCE_IP'/' dev-kubeconfig
+kubectl --kubeconfig dev-kubeconfig apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl --kubeconfig dev-kubeconfig version | base64 | tr -d '\n')"
