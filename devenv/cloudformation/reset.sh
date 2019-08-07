edd205f9e3dcf86225c4bb595bd2b8f8e993014a
Chuck Ha <chuckh@vmware.com>
2019-08-07 09:12:50 -0400

    Update reset and add dockercluster to example
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

--- devenv/cloudformation/reset.sh
+++ devenv/cloudformation/reset.sh
@@ -10,7 +10,9 @@ INSTANCE_ID=$(aws cloudformation describe-stacks --stack-name ClusterAPIDevEnvir
 INSTANCE_IP=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 
 ssh "ubuntu@${INSTANCE_IP}" 'sudo kubeadm reset -f --cri-socket /var/run/containerd/containerd.sock && sudo kubeadm init --config /tmp/kubeadm-config.yaml'
+ssh "ubuntu@${INSTANCE_IP}" 'sudo docker rm -f -v $(sudo docker ps -a -q) || echo "no containers to clean up"'
 ssh "ubuntu@${INSTANCE_IP}" 'sudo -s cat /etc/kubernetes/admin.conf' > dev-kubeconfig
+
 
 sed -i '' -E 's/([0-9]{1,3}\.){3}[0-9]{1,3}/'$INSTANCE_IP'/' dev-kubeconfig
 kubectl --kubeconfig dev-kubeconfig apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl --kubeconfig dev-kubeconfig version | base64 | tr -d '\n')"
